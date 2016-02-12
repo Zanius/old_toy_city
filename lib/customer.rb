5ea8f4966e917605c21ab764a306da4837a22f5f
Zane Brown <zib1991@gmail.com>
2016-02-12 18:15:43 -0500

    Added products and customers methods

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,2 +1,24 @@
 class Customer
+  attr_reader :name
+  @@customers = []
+
+  def initialize(options={})
+    @name = options[:name]
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+  def self.find_by_name(customer_name)
+    @@customers.select { |customer| customer.name == customer_name}[0]
+  end
+
+
+  private
+
+  def add_to_customers
+    @@customers << self
+  end
 end
--- lib/product.rb
+++ lib/product.rb
@@ -1,14 +1,28 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  def self.find_by_title(product_title)
+    @@products.select { |product| product.title == product_title}[0]
+  end
+
+  def in_stock?
+    @stock > 0
+  end
+
+  def self.in_stock
+    @@products.select {|product| product.stock > 0}
   end
 
   private
